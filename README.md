82d4f20194a26c99c2e13e53caa8e5e9a4ec8f25
masayasuzuki <msuzuki@masayasuzuki-no-Mac-mini.local>
2016-04-03 19:06:36 +0900

    implemented first commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+Android RecyclerView Demo
+===================================
+
+Sample demonstrating the use of RecyclerView. 
+Sample Code is useful to create your android app.
+
+Article & Code explanation
+------------
+The explanation of this code is publishing on website.
+
+http://java-lang-programming.com/
+
+Credits
+------------
+Developed by java-lang-programming (MasayaSuzuki) - paulburke.co
---
+++ app/src/main/java/com/java_lang_programming/android_recycleview_demo/ui/RecyclerViewFragment.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2016 Masaya Suzuki
+ *
+ *      http://java-lang-programming.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.java_lang_programming.android_recycleview_demo.ui;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.java_lang_programming.android_recycleview_demo.R;
+import com.java_lang_programming.android_recycleview_demo.ui.dummy.DummyContent;
+import com.java_lang_programming.android_recycleview_demo.ui.dummy.DummyContent.DummyItem;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class RecyclerViewFragment extends Fragment {
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private OnListFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public RecyclerViewFragment() {
+    }
+
+    public static RecyclerViewFragment newInstance(int columnCount) {
+        RecyclerViewFragment fragment = new RecyclerViewFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS, mListener));
+        }
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        void onListFragmentInteraction(DummyItem item);
+    }
+}
